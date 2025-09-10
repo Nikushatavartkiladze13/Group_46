a3e4d89cad59ed83adc7b997240f33846c793296
nikushatavartkiladze13 <nikatavartkiladze09@icloud.com>
2025-09-10 12:30:44 +0400

    Second commit

--- day_96/homework/index.js
+++ day_96/homework/index.js
@@ -8,11 +8,24 @@ function display(symbols) {
     result.value += symbols
 }
 
+function cButton() {
+    result.value = ""
+}
+
+
 function deleteValue() {
     if (result.value.length >= 1 && result.value !== "Empty") {
         result.value = result.value.slice(0, -1)
     } else {
         result.value = "Empty"
-        result.style.color = "red"
+    }
+}
+
+
+function EvenOdd() {
+    if (result.value % 2 == 0) {
+        result.value = "Even"
+    } else {
+        result.value = "Odd"
     }
 }
